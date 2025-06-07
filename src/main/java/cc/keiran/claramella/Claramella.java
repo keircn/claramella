2661fb9f0ce6f762cf8684fe4f181064244cf037
Keiran <me@keiran.cc>
2025-06-07 13:50:25 +0100

    feat: integrate warp system into main plugin

--- src/main/java/cc/keiran/claramella/Claramella.java
+++ src/main/java/cc/keiran/claramella/Claramella.java
@@ -3,15 +3,18 @@ package cc.keiran.claramella;
 import org.bukkit.plugin.java.JavaPlugin;
 import cc.keiran.claramella.commands.ConfigCommand;
 import cc.keiran.claramella.commands.AdminCommand;
+import cc.keiran.claramella.commands.WarpCommand;
 import cc.keiran.claramella.config.DatabaseManager;
 import cc.keiran.claramella.features.admin.AdminManager;
 import cc.keiran.claramella.features.sleep.SleepListener;
 import cc.keiran.claramella.features.welcome.WelcomeListener;
+import cc.keiran.claramella.features.warps.WarpManager;
 
 public class Claramella extends JavaPlugin {
 
     private DatabaseManager databaseManager;
     private AdminManager adminManager;
+    private WarpManager warpManager;
     private SleepListener sleepListener;
     private WelcomeListener welcomeListener;
 
@@ -21,6 +24,8 @@ public class Claramella extends JavaPlugin {
         databaseManager.initialize();
         
         adminManager = new AdminManager(this, databaseManager);
+        warpManager = new WarpManager(this, databaseManager);
+        warpManager.initialize();
         
         welcomeListener = new WelcomeListener(this, databaseManager);
         sleepListener = new SleepListener(this, databaseManager);
@@ -36,6 +41,10 @@ public class Claramella extends JavaPlugin {
         AdminCommand adminCommand = new AdminCommand(this, databaseManager, adminManager);
         getCommand("admin").setExecutor(adminCommand);
         getCommand("admin").setTabCompleter(adminCommand);
+        
+        WarpCommand warpCommand = new WarpCommand(this, databaseManager, warpManager);
+        getCommand("warp").setExecutor(warpCommand);
+        getCommand("warp").setTabCompleter(warpCommand);
 
         if (databaseManager.getConfigValue("plugin.debug_mode", Boolean.class, false)) {
             getLogger().info("Debug mode enabled");
@@ -59,5 +68,9 @@ public class Claramella extends JavaPlugin {
     
     public AdminManager getAdminManager() {
         return adminManager;
+    }
+    
+    public WarpManager getWarpManager() {
+        return warpManager;
     }
 }
